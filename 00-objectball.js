28c7f99c0e0e51d71ce085f11fd0c114b4cf7a39
Dean Baberowski <deanbaberows@gmail.com>
2025-05-14 16:08:48 +1000

    complete lab

---
+++ 00-objectball.js
@@ -0,0 +1,125 @@
+function gameObject() {
+    return {
+        home: {
+            teamName: "Brooklyn Nets",
+            colors: ["Black", "White"],
+            players: {
+                "Alan Anderson": {
+                    number: 0,
+                    shoe: 16,
+                    points: 22,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 1
+                },
+                "Reggie Evans": {
+                    number: 30,
+                    shoe: 14,
+                    points: 12,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 12,
+                    blocks: 12,
+                    slamDunks: 7
+                },
+                "Brook Lopez": {
+                    number: 11,
+                    shoe: 17,
+                    points: 17,
+                    rebounds: 19,
+                    assists: 10,
+                    steals: 3,
+                    blocks: 1,
+                    slamDunks: 15
+                },
+                "Mason Plumlee": {
+                    number: 1,
+                    shoe: 19,
+                    points: 26,
+                    rebounds: 12,
+                    assists: 6,
+                    steals: 3,
+                    blocks: 8,
+                    slamDunks: 5
+                },
+                "Jason Terry": {
+                    number: 31,
+                    shoe: 15,
+                    points: 19,
+                    rebounds: 2,
+                    assists: 2,
+                    steals: 4,
+                    blocks: 11,
+                    slamDunks: 1
+                }
+            }
+        },
+        away: {
+            teamName: "Charlotte Hornets",
+            colors: ["Turquoise", "Purple"],
+            players: {
+                "Jeff Adrien": {
+                    number: 4,
+                    shoe: 18,
+                    points: 10,
+                    rebounds: 1,
+                    assists: 1,
+                    steals: 2,
+                    blocks: 7,
+                    slamDunks: 2
+                },
+                "Bismak Biyombo": {
+                    number: 0,
+                    shoe: 16,
+                    points: 12,
+                    rebounds: 4,
+                    assists: 7,
+                    steals: 7,
+                    blocks: 15,
+                    slamDunks: 10
+                },
+                "DeSagna Diop": {
+                    number: 2,
+                    shoe: 14,
+                    points: 24,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 4,
+                    blocks: 5,
+                    slamDunks: 5
+                },
+                "Ben Gordon": {
+                    number: 8,
+                    shoe: 15,
+                    points: 33,
+                    rebounds: 3,
+                    assists: 2,
+                    steals: 1,
+                    blocks: 1,
+                    slamDunks: 0
+                },
+                "Brendan Haywood": {
+                    number: 33,
+                    shoe: 15,
+                    points: 6,
+                    rebounds: 12,
+                    assists: 12,
+                    steals: 22,
+                    blocks: 5,
+                    slamDunks: 12
+                }
+            }
+        }
+    };
+}
+
+console.log(gameObject());
+// The gameObject function returns an object representing a basketball game.
+// The object contains two teams: home and away.
+// Each team has a name, colors, and a list of players.
+// Each player has various statistics such as number, shoe size, points, rebounds, assists, steals, blocks, and slam dunks.
+// The gameObject function is called at the end to log the object to the console.
+// The output will show the structure of the game object with all the teams and players.
+// The gameObject function is a representation of a basketball game object.
